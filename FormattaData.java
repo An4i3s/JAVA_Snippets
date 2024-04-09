4afd6e58f4aaaed2fd05572ec39738b4720ff8f2
Anaïs <76781733+An4i3s@users.noreply.github.com>
2024-04-09 12:56:08 +0200

    Aggiunto formato con Calendar

--- FormattaData.java
+++ FormattaData.java
@@ -21,7 +21,23 @@ public class FormattaData {
     //metodi get
     //System.out.println(d.getDayOfWeek()+" "+d);
     
-
+  
+           Calendar day1 = calendar;
+        String day1String = formatCalendarDate(day1);
+        Calendar day2 = (Calendar) calendar.clone();
+        day2.add(Calendar.DAY_OF_MONTH, 1);
+        String day2String = formatCalendarDate(day2);
+        Calendar day3 = (Calendar) calendar.clone();
+        day3.add(Calendar.DAY_OF_MONTH, 2);
+        String day3String = formatCalendarDate(day3);
+        Calendar day4 = (Calendar) calendar.clone();
+        day4.add(Calendar.DAY_OF_MONTH, 3);
+        Calendar day5 = (Calendar) calendar.clone();
+        day5.add(Calendar.DAY_OF_MONTH, 4);
+        Calendar day6 = (Calendar) calendar.clone();
+        day6.add(Calendar.DAY_OF_MONTH, 5);
+        Calendar day7 = (Calendar) calendar.clone();
+        day7.add(Calendar.DAY_OF_MONTH, 6);
 
     
 }
